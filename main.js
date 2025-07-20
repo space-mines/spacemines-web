366ebf2ddca2c3eb95eb895c5f735903be044c1c
twcrone <twcrone@gmail.com>
2025-07-20 13:37:07 -0400

    Back up camera

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const canvas = document.querySelector('#c');
 const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight,
-    0.1, 15 );
+    0.1, 50 );
 const color = 0xFFFFFF;
 const intensity = 3;
 const light = new THREE.DirectionalLight(color, intensity);
@@ -16,26 +16,27 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-function create(geometry, color, x) {
+function create(geometry, color, x, y, z) {
     const material = new THREE.MeshLambertMaterial({color});
 
     const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
 
     cube.position.x = x;
-    cube.position.z = -10;
+    cube.position.y = y;
+    cube.position.z = z;
 
     return cube;
 }
 
 const geometry = new THREE.IcosahedronGeometry( 1 );
 const cubes = [
-    create(geometry, 0x44aa88,  0),
-    create(geometry, 0x8844aa, -2),
-    create(geometry, 0xaa8844,  2),
+    create(geometry, 0x44aa88,  0, 0, 0),
+    create(geometry, 0x8844aa, -5, -5, -5),
+    create(geometry, 0xaa8844,  5, 5, 5),
 ];
 
-camera.position.z = 5;
+camera.position.z = 30;
 
 function animate() {
 
