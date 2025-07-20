70614a7900d2ce51d4c322a9819dad466d8066ec
twcrone <twcrone@gmail.com>
2025-07-20 07:58:50 -0400

    Add second cube

--- main.js
+++ main.js
@@ -8,24 +8,40 @@ const intensity = 3;
 const light = new THREE.DirectionalLight(color, intensity);
 const renderer = new THREE.WebGLRenderer();
 
-light.position.set(-1, 2, 4);
+light.position.set(-1, 2, 10);
 scene.add(light);
 
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshPhongMaterial( { color: 0x44aa88 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+function create(geometry, color, x) {
+    const material = new THREE.MeshPhongMaterial({color});
 
-camera.position.z = 2;
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    cube.position.x = x;
+
+    return cube;
+}
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const cube1 = create(geometry, 0x44aa88, -1);
+const cube2 = create(geometry, 0x44aa88, 1);
+
+scene.add( cube1 );
+
+camera.position.z = 5;
 
 function animate() {
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube1.rotation.x += 0.01;
+    cube1.rotation.y += 0.01;
+
+    cube2.rotation.x += 0.01;
+    cube2.rotation.y += 0.01;
 
     renderer.render( scene, camera );
 }
+
