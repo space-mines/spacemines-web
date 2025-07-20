ef87a5f5a5676e03de375d3a5b3093719d5bc44a
twcrone <twcrone@gmail.com>
2025-07-20 07:52:06 -0400

    Add light and use material that takes light

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight,
+    0.1, 5 );
+const color = 0xFFFFFF;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
 const renderer = new THREE.WebGLRenderer();
+
+light.position.set(-1, 2, 4);
+scene.add(light);
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshPhongMaterial( { color: 0x44aa88 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 2;
 
 function animate() {
 
